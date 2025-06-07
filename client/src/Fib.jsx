416f237161e121c6236019e3f755b3944221d621
amol <amolshrivastava448@gmail.com>
2025-06-07 17:01:55 +0530

    Made changes to run with k8s setup as well

--- client/src/Fib.jsx
+++ client/src/Fib.jsx
@@ -1,77 +1,98 @@
-import axios from 'axios';
-import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Fib = () => {
-    const [seenIndexes, setSeenIndexes] = useState([]);
-    const [values, setValues] = useState({});
-    const [index, setIndex] = useState('');
+  const [seenIndexes, setSeenIndexes] = useState([]);
+  const [values, setValues] = useState({});
+  const [index, setIndex] = useState("");
 
-    useEffect(() => {
-        fetchValues();
-        fetchIndexes()
-    }, [])
+  useEffect(() => {
+    fetchValues();
+    fetchIndexes();
+  }, []);
 
-    const fetchValues = async () =>{
-        const {data} = await axios.get('/api/values/current');
-        setValues({values: data})
+  const fetchValues = async () => {
+    const { data } = await axios.get("/api/values/current");
+    //setValues({values: data});
+    setValues(data);
+  };
+
+  const fetchIndexes = async () => {
+    const { data } = await axios.get("/api/values/all");
+    //setSeenIndexes({
+    //     seenIndexes: data
+    //    });
+    setSeenIndexes(data);
+  };
+
+  const renderSeenIndexes = () => {
+    // if(seenIndexes && seenIndexes.seenIndexes && seenIndexes.seenIndexes.length)
+    //     return seenIndexes.seenIndexes.map(el => el.number).join(', ');
+    // return (<div> No calculation stored. </div>)
+    if (Array.isArray(seenIndexes) && seenIndexes.length > 0)
+      return seenIndexes.map((el) => el.number).join(", ");
+    return <div>No calculation stored.</div>;
+  };
+
+  const renderCalculatedValues = () => {
+    // const entries = [];
+
+    // if(values && values.values) {
+    //     for(let key in values.values) {
+    //         entries.push(
+    //             <div key={key}>
+    //                 For index {key} I calculated {values.values[key]}
+    //             </div>
+    //         )
+    //     }
+
+    //     return entries;
+    // }
+
+    // return (<div> No calculation stored.</div>)
+
+    const entries = [];
+
+    for (let key in values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {values[key]}
+        </div>
+      );
     }
 
-    const fetchIndexes = async () => {
-        const {data} = await axios.get('/api/values/all');
-        setSeenIndexes({
-            seenIndexes: data
-        })
-    }
+    return entries.length ? entries : <div>No calculation stored.</div>;
+  };
 
-    const renderSeenIndexes = () => {
-        if(seenIndexes && seenIndexes.seenIndexes && seenIndexes.seenIndexes.length)
-            return seenIndexes.seenIndexes.map(el => el.number).join(', ');
-        return (<div> No calculation stored. </div>)
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const renderCalculatedValues = () => {
-        const entries = [];
+    await axios.post("/api/values", {
+      index,
+    });
 
-        if(values && values.values) {
-            for(let key in values.values) {
-                entries.push(
-                    <div key={key}>
-                        For index {key} I calculated {values.values[key]}
-                    </div>
-                )
-            }
-    
-            return entries;
-        }
-        
-        return (<div> No calculation stored.</div>)
-    }
+    setIndex("");
+  };
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-
-        await axios.post('/api/values',  {
-            index
-        })
-
-        setIndex('')
-    }
-
-    return (
+  return (
     <div>
-        <form onSubmit={handleSubmit}>
-            <label>Enter your index: </label>
-            <input type='number' value={index} onChange={(e) => setIndex(e.target.value)}></input>
-            <button>Submit</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index: </label>
+        <input
+          type="number"
+          value={index}
+          onChange={(e) => setIndex(e.target.value)}
+        ></input>
+        <button>Submit</button>
+      </form>
 
-        <h3>Indexes I have seen </h3>
-        {renderSeenIndexes()}
+      <h3>Indexes I have seen </h3>
+      {renderSeenIndexes()}
 
-        <h3> Calculated Values: </h3>
-        {renderCalculatedValues()}
+      <h3> Calculated Values: </h3>
+      {renderCalculatedValues()}
     </div>
-  )
-}
+  );
+};
 
 export default Fib;
